fb3f9b60c520e69a1112489f528c0b985d2bd7fc
vikash101989 <39409363+vikash101989@users.noreply.github.com>
2022-03-06 15:30:07 +0530

    add config folder

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['protocol']     = "smtp"; 
+$config['smtp_host']    = "ssl://smtp.aortadhc.com";
+$config['smtp_user']    = "info@aortadhc.com"; 
+$config['smtp_pass']    = "cF-;S5eh,wkX";
+$config['smtp_port']    =  465;
+$config['smtp_crypto']  = 'ssl';
+$config['mailtype']     = "html";
+$config['charset']      = "iso-8859-1";
+$config['newline']      = "\r\n";
+$config['wordwrap']     = TRUE;
